945b436cf4ac9f8b1ef2e932fb8d3682a68fb8d9
Mackenzie Gray <mdkulen@gmail.com>
2022-12-08 15:09:19 +1100

    added useState functions and component files for game

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,32 @@
+import { useState } from "react";
+import { HangmanDrawing } from "./HangmanDrawing";
+import { HangmanWord } from "./HangmanWord";
+import { Keyboard } from "./Keyboard";
+import words from "./wordList.json";
+
 function App() {
+  const [wordToGuess, setWordToGuess] = useState(() => {
+    return words[Math.floor(Math.random() * words.length)]
+  });
+
+  const [guessedLetters, setGuessedLetters] = useState<string[]>([]);
+
   return (
-    <h1>Hi</h1>
+    <div style={{
+      maxWidth: '800px',
+      display: 'flex',
+      flexDirection: 'column',
+      gap: '2rem',
+      margin: '0, auto',
+      alignItems: 'center'
+    }}
+    >
+      <div style={{ fontSize: '2rem', textAlign: 'center'}}>Lose Win</div>
+      <HangmanDrawing />
+      <HangmanWord />
+      <Keyboard />
+
+    </div>
   )
 }
 
