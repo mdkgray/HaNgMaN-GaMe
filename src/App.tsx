81962ae98a24a4f18d15f298f871ccadda3c4e08
Mackenzie Gray <mdkulen@gmail.com>
2022-12-08 17:27:19 +1100

    added win/lose statement to page

--- src/App.tsx
+++ src/App.tsx
@@ -13,12 +13,18 @@ function App() {
 
   const incorrectLetters = guessedLetters.filter(letter => !wordToGuess.includes(letter));
 
+  // Variables to determine if winner or loser 
+  const isLoser = incorrectLetters.length >= 6;
+  const isWinner = wordToGuess.split("").every(letter => guessedLetters.includes(letter));
+
+  // Callback function to add guessed letters to array
   const addGuessedLetter = useCallback((letter: string) => {
     if (guessedLetters.includes(letter)) return;
 
     setGuessedLetters(currentLetters => [...currentLetters, letter])
   }, [guessedLetters]);
   
+  // useEffect function to add event listener to keyboard press 
   useEffect(() => {
     const handler = (e: KeyboardEvent) => {
       const key = e.key;
@@ -44,9 +50,14 @@ function App() {
       gap: '2rem',
       margin: '0, auto',
       alignItems: 'center'
-    }}
-    >
-      <div style={{ fontSize: '2rem', textAlign: 'center'}}>Lose Win</div>
+    }}>
+      <div style={{ 
+        fontSize: '2rem', 
+        textAlign: 'center'
+        }}>
+          {isWinner && "You have won! Refresh the page to try again."}
+          {isLoser && "Nice try...Refresh the page to try again."}
+      </div>
       <HangmanDrawing numberOfGuesses={incorrectLetters.length}/>
       <HangmanWord guessedLetters={guessedLetters} wordToGuess={wordToGuess} />
       <div style={{ alignSelf: 'stretch'}}>
@@ -54,7 +65,7 @@ function App() {
           activeLetters={guessedLetters.filter(letter => wordToGuess.includes(letter))}
           inactiveLetters={incorrectLetters}
           addGuessedLetter={addGuessedLetter}
-          />
+        />
       </div>      
     </div>
   )
